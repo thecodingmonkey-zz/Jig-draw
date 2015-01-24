a49ad847cc3d85393a48c5881deb554a34eeee6a
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-24 01:06:48 -1000

    put in basic functionality to change word every 60s. still need to handle subwords, and buttons to change subword/main word manually.

--- words.js
+++ words.js
@@ -1,9 +1,2 @@
 var words = [
-// format: first element is the main word, all other elements are clues
-  ['computer', 'monitor', 'keyboard', 'mouse'],
-  ['griffon', 'claw', 'wings', 'mane', 'tail'],
-  ['hula girl', 'torso', 'grass', 'coconuts', 'hands', 'skirt'],
-  ['night', 'moon', 'stars', 'owl', 'black', 'satelite', 'darkness'],
-  ['chinaman', 'gong', 'wok', 'rice hat', 'buckteeth', 'chopsticks', 'boat', 'rice bowl'],
-  ['universe', 'milky way', 'spaceship', 'alien', 'stars', 'earth', 'sun', 'jupiter']
 ];
