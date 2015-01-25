b84dba3b58129937fdf4e3f0e19f2786e2175979
Alan Solidum <asolidum+bitbucket@gmail.com>
2015-01-25 01:05:02 -1000

    Added collaborative quickdraw mode

---
+++ words-test.js
@@ -0,0 +1,18 @@
+window.library = {
+  words_with_clues:
+  [
+// format: first element is the main word, all other elements are clues
+    ['Aladdin', 'Abu (sidekick monkey)', 'magic carpet', 'lamp', 'Princess Jasmine', 'genie', 'Disney'],
+    ['Nascar', 'circle track', 'race cars', 'trophy', 'checkered flag', 'finish line', 'crowd stands'],
+    ['Star Wars', 'death star', 'R2D2', 'x-wing',  'light saber', 'Yoda', 'Jabba the Hut']
+  ],
+  words:
+  [
+    ['George Washington', 'consititution', 'colonial wig', 'cherry tree', 'ax', 'wooden teeth', 'The White House', 'American Flag', 'dollar bill'],
+    ['Charlie Chaplin', 'cane', 'mustach (hitler-stache)', 'bowler hat', 'smile', 'suit', 'curly hair'],
+    ['Thanksgiving', 'turkey', 'television', 'parade', 'pumpkin', 'table', 'family' ],
+    ['Scooby Doo', 'clue', 'clue', 'clue', 'clue', 'clue', 'clue' ],
+    ['Pokemon', 'clue', 'clue', 'clue', 'clue', 'clue', 'clue' ],
+    ['James Bond', 'clue', 'clue', 'clue', 'clue', 'clue', 'clue' ]
+  ]
+};
