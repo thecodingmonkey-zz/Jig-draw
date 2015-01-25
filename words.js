bef310da4803c6af68860c595310a41dee30dec7
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-24 14:53:51 -1000

    sanatize data.  fix up code

--- words.js
+++ words.js
@@ -1,4 +1,4 @@
-window.library ={
+window.library = {
   words: 
   [
 // format: first element is the main word, all other elements are clues
@@ -9,14 +9,14 @@ window.library ={
     ['chinaman', 'gong', 'wok', 'rice hat', 'buckteeth', 'chopsticks', 'boat', 'rice bowl'],
     ['universe', 'milky way', 'spaceship', 'alien', 'stars', 'earth', 'sun', 'jupiter'],
     ['vehicle', 'car', 'truck', 'cart', 'sled', 'tractor', 'train', 'plane', 'boat'],
-    ['cold', 'gloves', 'scarf', 'jacket', 'earmuffs', 'snow', 'hat', 'boots', 'ice',
+    ['cold', 'gloves', 'scarf', 'jacket', 'earmuffs', 'snow', 'hat', 'boots', 'ice'],
     ['mammal', 'man', 'elephant', 'whale', 'dog', 'panda', 'kangaroo', 'lion', 'monkey'],
     ['Up', 'house', 'adventure book', 'dog', 'cliffs with waterfall', 'giant bird egg', 'old man walker', 'cone of shame', 'hot air balloon'],
-    ['titanic', 'ocean', 'captain\'s hat', 'life saver', 'rose', 'heart of the ocean' 'necklace', 'ship', 'iceberg', 'life vest'],
+    ['titanic', 'ocean', 'captain\'s hat', 'life saver', 'rose', 'heart of the ocean', 'necklace', 'ship', 'iceberg', 'life vest'],
     ['Harry Potter and the Goblet of Fire', 'wand', 'dragon', 'train', 'mermaid', 'Hogwartz',  'thunderbolt', 'broomstick', 'goblet of fire'],
     ['Google', 'world', 'computer', 'glasses (google glass)', 'map', 'magnifying glass', 'search bar', 'office', 'chrome icon'],
     ['Great Britain', 'tea', 'telephone booth', 'magna carta', 'Sherlock Holmes', 'Big Ben', 'crown', 'London Bridge', 'palace'],
-    ['Charlie Chaplin', 'cane', 'movie theater', 'laughing', 'mustach (hitler-stache'), 'bowler hat', 'smile', 'suit', 'curly hair'],
+    ['Charlie Chaplin', 'cane', 'movie theater', 'laughing', 'mustach (hitler-stache', 'bowler hat', 'smile', 'suit', 'curly hair'],
     ['George Washington', 'consititution', 'colonial wig', 'cherry tree', 'ax', 'wooden teeth', 'The White House', 'American Flag', 'dollar bill'],
     ['Aladdin', 'Agrabah', 'Princess Jasmine', 'Abu (sidekick monkey)', 'magic carpet', 'lamp', 'genie', 'turbin', 'Disney'],
     ['New York', 'taxi', 'Empire State Building', 'Times Square', 'New Years Ball Drop', 'Twin Towers', 'airplane', 'large apple', 'statue of liberty'],
@@ -35,6 +35,6 @@ window.library ={
     ['Prius', 'Electricity', 'car', 'Toyota', 'lightening', 'charging station', 'hippie', 'recycling', 'hatchback'],
     ['Air Force One', 'airplane', 'White House', 'Washington DC', 'American Flag', 'Obama entering airplane', 'hashtag', 'Air Force', 'eagle'],
     ['Aladdin', 'princess', 'monkey', 'flying carpet', 'lamp', 'genie', 'Aladdin', 'Mickey Mouse', 'movie'],
-    ['Waikiki', 'beach', 'ocean', 'surfers', 'canoe', 'Diamondhead', 'Duke's statue', 'hula dancer', 'Oahu']
+    ['Waikiki', 'beach', 'ocean', 'surfers', 'canoe', 'Diamondhead', 'Duke\'s statue', 'hula dancer', 'Oahu']
   ]  
 }
